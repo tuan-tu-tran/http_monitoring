bed55fc864553ab1bcd1a8251f883c7971fa8b28
Tuan-Tu Tran <tuantu.t@gmail.com>
2017-05-18 00:21:59 +0200

    dump the content into template if not exists

--- monitor.py
+++ monitor.py
@@ -27,13 +27,22 @@ def main():
 	process(url, template)
 
 def process(url, templateFile):
-	with open(templateFile) as fh:
-		template=fh.read()
+	import os
+	if os.path.exists(templateFile):
+		with open(templateFile) as fh:
+			template=fh.read()
+	else:
+		template=None
 	import urllib2
 	import datetime
 	resp=urllib2.urlopen(url)
 	page=resp.read()
 	resp.close()
+	if template==None:
+		with open(templateFile,"w") as fh:
+			fh.write(page)
+			logger.info("wrote content of %s to %s", url, templateFile)
+		return
 	if page!=template:
 		log=logger.warn
 		ok="not ok"
